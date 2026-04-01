770e17f17ebb5c2d90d3022a3f26da11aa923cfb
czj <1350862402@qq.com>
2026-04-01 17:19:31 +0800

    切掉无关的重导入链

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,8 @@
-from src.utils.pylogger import get_pylogger
+def get_pylogger(*args, **kwargs):
+    from src.utils.pylogger import get_pylogger as _get_pylogger
+    return _get_pylogger(*args, **kwargs)
+
+__all__ = ["get_pylogger"]
+
 from src.utils.rich_utils import enforce_tags, print_config_tree
 from src.utils.utils import *
